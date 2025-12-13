791ae13701b538ddcdddd0d09bff79158ffec6a0
Kraskaska <k.krasilnikov.2008@gmail.com>
2025-12-13 10:07:26 -0600

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pushd $(dirname $0)
-git pull && docker build . -t borispetrovich:latest && (docker compose down || true) && docker compose run app migrate && docker compose up -d
+git pull && docker build . -t borispetrovich:latest && (docker compose down || true) && docker compose run --rm app migrate && docker compose up -d --no-recreate
 popd
